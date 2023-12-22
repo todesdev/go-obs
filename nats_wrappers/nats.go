75cdc331b6edb686b19ef8b770e71b84a6f10c62
Vladimirs Duhovs <vladimirs.duhovs@todes.group>
2023-12-22 21:50:15 +0200

    fix: removed wait groups

--- nats_wrappers/nats.go
+++ nats_wrappers/nats.go
@@ -6,7 +6,6 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
 	"go.uber.org/zap"
-	"sync"
 	"time"
 
 	"github.com/nats-io/nats.go"
@@ -15,26 +14,24 @@ import (
 
 type SubscribeHandler func(msg *nats.Msg, ctxOpts ...context.Context) error
 
-func SubscribeWithObservability(ctx context.Context, wg *sync.WaitGroup, stream nats.JetStream, subject, queue string, handler SubscribeHandler, opts ...nats.SubOpt) (*nats.Subscription, error) {
+func SubscribeWithObservability(ctx context.Context, stream nats.JetStream, subject, queue string, handler SubscribeHandler, opts ...nats.SubOpt) (*nats.Subscription, error) {
 	sub, err := stream.QueueSubscribeSync(subject, queue, opts...)
 	if err != nil {
 		return nil, err
 	}
 	natsCollector := natscollector.GetNATSCollector()
 
-	err = handleSubscription(ctx, wg, sub, handler, natsCollector)
+	err = handleSubscription(ctx, sub, handler, natsCollector)
 	return sub, err
 }
 
-func handleSubscription(ctx context.Context, wg *sync.WaitGroup, sub *nats.Subscription, handler SubscribeHandler, natsCollector *natscollector.NATSCollector) error {
+func handleSubscription(ctx context.Context, sub *nats.Subscription, handler SubscribeHandler, natsCollector *natscollector.NATSCollector) error {
 	for {
 		select {
 		case <-ctx.Done():
 			return sub.Unsubscribe()
 		default:
 		}
-		wg.Add(1)
-		defer wg.Done()
 
 		msg, err := sub.NextMsgWithContext(ctx)
 		if err != nil {
